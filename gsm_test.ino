24b13a669580e155749d59160747635ccc2a26af
venkhatesh <venkhatesharunachalam@gmail.com>
2018-08-07 21:34:01 +0530

    GSM Test Program

---
+++ gsm_test.ino
@@ -0,0 +1,47 @@
+#include<SoftwareSerial.h>
+
+SoftwareSerial mySerial(9, 10);
+
+void setup() {
+  // put your setup code here, to run once:
+  mySerial.begin(9600);
+  Serial.begin(9600);
+  delay(100);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>0)
+    switch(Serial.read())
+    {
+        case 's':
+          SendMessage();
+          break;
+        case 'r':
+          RecieveMessage();
+          break;
+    }
+
+    if (mySerial.available()>0)
+      Serial.write(mySerial.read());
+}
+
+void SendMessage(){
+
+  mySerial.println("AT+CMGF=1");
+  delay(1000);
+  mySerial.println("AT+CMGS=\"+919892466035\"\r");
+  delay(1000);
+  mySerial.println("Hello World !!");
+  mySerial.println("Sent from GSM Module");
+  delay(100);
+  mySerial.println((char)26);
+  delay(1000);
+}
+
+void RecieveMessage(){
+  mySerial.println("AT+CNMI=2,2,0,0,0");
+  delay(1000);
+}
+
